711af6753fe3fe0f19e48425fbeb0e0a9594634d
Silvia Juliana Pabón Chacón <41413594+SilviaPabon24@users.noreply.github.com>
2026-08-11 15:48:36 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,4 +6,4 @@
 // 2. Pega aquí la URL que te entrega Apps Script al publicar
 //    el Web App (termina en /exec).
 // ============================================================
-const APPS_SCRIPT_URL = "https://script.google.com/macros/library/d/1SfXdDi2kRVIj1ltreWUN8AAkHUp2LJUGIBJxoPdp73saAuCPKXxl2S_0/1";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_NCQq4B4HkTjlNAYbwrsMSXtChnC6S09ECmw6p9KEBCiSYjcNNCBpQrxONF_YAxDm-g/exec";
